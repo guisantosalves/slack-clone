eeabf2856f8c385f61515f5f655fb95f72210c95
guisantosalves <workguisantos@gmail.com>
2022-07-26 21:25:24 -0400

    create sidebar header

---
+++ src/components/SideBarOptions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const SideBarOption = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default SideBarOption;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { FiberManualRecord } from "@mui/icons-material";
+import { Create } from "@mui/icons-material";
 
 const SideBar = () => {
     return(
         <SideBarContainer>
             {/* side bar header */}
-            <h1>i am the side bar</h1>
+            <SideBarHeader>
+                <SideBarInfo>
+                    <h2>Guizao</h2>
+                    <h3>
+                        <FiberManualRecord/>
+                        guilherme
+                    </h3>
+                </SideBarInfo>
+                <Create />
+            </SideBarHeader>
         </SideBarContainer>
     )
 }
@@ -13,5 +24,48 @@ const SideBar = () => {
 export default SideBar;
 
 const SideBarContainer = styled.div`
-        background-color: var(--slack-color);
+    flex: 0.3;
+    background-color: var(--slack-color);
+    color: white;
+    /* amount of width the side-bar will use. */
+    max-width: 260px;
+    margin-top: 60px;
+    border: 1px solid #49274b;
+`;
+
+const SideBarHeader = styled.div`
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
+
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 999px;
+    }
+`;
+
+const SideBarInfo = styled.div`
+    flex: 1;
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
 `;
