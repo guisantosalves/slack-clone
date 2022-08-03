24168c25a5b6321c705bd6a5257e58d25a87ddf5
guisantosalves <workguisantos@gmail.com>
2022-08-02 20:54:07 -0400

    working on header of chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
+// material UI
+import { StarBorderOutlined } from "@mui/icons-material";
+import { InfoOutlined } from "@mui/icons-material";
+
 const Chat = () => {
     return (
         <ChatContainer>
-            <h2>I am the chat</h2>
-            <h2>I am the chat</h2>
-            <h2>I am the chat</h2>
-            <h2>I am the chat</h2>
-            <h2>I am the chat</h2>
-            <h2>I am the chat</h2>
+            <Header>
+                <HeaderLeft>
+                    <h4>
+                        <strong>#Room name</strong>
+                    </h4>
+                    <StarBorderOutlined />
+                </HeaderLeft>
+                <HeaderRight>
+                    <p>
+                        <InfoOutlined /> Details
+                    </p>
+                </HeaderRight>
+            </Header>
         </ChatContainer>
     )
 }
@@ -22,4 +33,20 @@ const ChatContainer = styled.div`
     flex: 0.7;
     flex-grow: 1;
     overflow-y: scroll;
+    margin-top: 60px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+
+`;
+
+const HeaderRight = styled.div`
+
 `;
